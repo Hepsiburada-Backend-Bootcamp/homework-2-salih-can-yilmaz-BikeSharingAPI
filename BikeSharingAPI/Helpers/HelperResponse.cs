f65dcb7c05d566a9835cd256fe20dd4779e16ca1
Can YILMAZ <canyilmaz999@gmail.com>
2021-10-02 23:07:55 +0300

    HelperResponse done

--- BikeSharingAPI/Helpers/HelperResponse.cs
+++ BikeSharingAPI/Helpers/HelperResponse.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Text.Json;
 using BikeSharingAPI.Enums;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,11 +11,58 @@ namespace BikeSharingAPI.Helpers
         public static IActionResult GenerateResponse<T>(
             EnumResponseFormat format,
             HttpStatusCode statusCode,
-            T data
+            T data,
+            bool isRawData = false
         )
         {
             if (data != null)
             {
+                string content = String.Empty;
+                switch (format)
+                {
+                    case EnumResponseFormat.JSON:
+                        if (!isRawData)
+                            content = JsonSerializer.Serialize(data);
+                        else
+                            content = data.ToString();
+
+                        return new ContentResult
+                        {
+                            StatusCode = Convert.ToInt32(statusCode),
+                            ContentType = "application/json",
+                            Content = content
+                        };
+
+                    case EnumResponseFormat.XML:
+                        if (!isRawData)
+                            content = HelperXML.TOXML<T>(data);
+                        else
+                            content = data.ToString();
+
+                        return new ContentResult
+                        {
+                            StatusCode = Convert.ToInt32(statusCode),
+                            ContentType = "application/xml",
+                            Content = content,
+                        };
+
+                    case EnumResponseFormat.HTML:
+                        return new ContentResult
+                        {
+                            StatusCode = Convert.ToInt32(statusCode),
+                            ContentType = "text/html",
+                            Content = data.ToString()
+                        };
+                    case EnumResponseFormat.TEXT:
+                        return new ContentResult
+                        {
+                            StatusCode = Convert.ToInt32(statusCode),
+                            ContentType = "text/plain; charset=UTF-8",
+                            Content = data.ToString()
+                        };
+                    default:
+                        break;
+                }
                 return new ContentResult
                 {
                     StatusCode = Convert.ToInt32(statusCode),
